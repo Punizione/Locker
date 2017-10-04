f2ed64a438e8a4ab9796235b9b1c833968ced9d9
Punizione <1273435147@qq.com>
2017-10-04 16:24:09 +0800

    fix some bug

--- app/src/main/java/com/delitto/locker/Struct/DataBaseHelper.java
+++ app/src/main/java/com/delitto/locker/Struct/DataBaseHelper.java
@@ -1,16 +1,26 @@
 package com.delitto.locker.Struct;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.delitto.locker.Tools.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Delitto on 2017/10/4.
  */
 
 public class DataBaseHelper extends SQLiteOpenHelper {
+    private static final String TAG = "DataBaseHelper";
     private static final String DatabaseName = "locker.db";
     private static final int version = 1;
+
+    private SQLiteDatabase db = null;
     public DataBaseHelper(Context context){
         super(context, DatabaseName, null, version);
     }
@@ -31,4 +41,96 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         //TODO
     }
 
+    public void insert(String packageName, long lestTime, long cycleTime, long ruleLessTime, long ruleCycleTime){
+        if(db==null){
+            db = this.getWritableDatabase();
+        }
+        String sql = "insert into rule(packagename, lesstime, cycletime, rulelesstime, rulecycletime) values(?,?,?,?,?)";
+        try{
+            db.execSQL(sql, new Object[]{packageName, lestTime, cycleTime, ruleLessTime, ruleCycleTime});
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.e(TAG, "insert error");
+        }finally {
+            db.close();
+        }
+        db = null;
+    }
+
+    public void delete(String packageName){
+        if(db==null){
+            db = this.getWritableDatabase();
+        }
+        String sql = "delete from rule where packagename = '"+packageName+"'";
+        try{
+            db.execSQL(sql);
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.e(TAG, "delete error");
+        }finally {
+            db.close();
+        }
+        db = null;
+    }
+
+    public void update(int filter, String packageName, long data)throws Exception{
+        if(db == null){
+            db = this.getWritableDatabase();
+        }
+        String dataFrom;
+        switch (filter){
+            case Constants.UPDATE_LESSTIME:
+                dataFrom = "lesstime";
+                break;
+            case Constants.UPDATE_CYCLETIME:
+                dataFrom = "cycletime";
+                break;
+            case Constants.UPDATE_RULE_LESSTIME:
+                dataFrom = "rulelesstime";
+                break;
+            case Constants.UPDATE_RULE_CYCLETIME:
+                dataFrom = "rulecycletime";
+                break;
+            default:
+                throw new Exception("Illegal Parameter");
+        }
+        // update rule set dateFrom = 'data' where packagename = 'packagename'
+        String sql = "update rule set "+dataFrom+" = '"+data+"' where packagename = '"+packageName+"'";
+        try{
+            db.execSQL(sql);
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.e(TAG, "update error");
+        }finally {
+            db.close();
+        }
+        db = null;
+    }
+
+    public Cursor select(){
+        if(db==null){
+            db = this.getReadableDatabase();
+        }
+        Cursor c = null;
+        try{
+            c = db.query("rule",null,null,null,null,null,null,null);
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.e(TAG, "select error");
+        }finally {
+            db.close();
+        }
+        db = null;
+        return c;
+    }
+    public List<Rule> getAllRules(){
+        Cursor c = select();
+        List<Rule> list = new ArrayList<>();
+        if(c.moveToFirst()){
+            for(int i=0;i<c.getCount();i++){
+                //TODO
+            }
+        }
+        return list;
+    }
 }
--- app/src/main/java/com/delitto/locker/Tools/Constants.java
+++ app/src/main/java/com/delitto/locker/Tools/Constants.java
@@ -6,16 +6,21 @@ package com.delitto.locker.Tools;
 
 public class Constants {
     //Applicaton Filter
-    public static int SYSTEM_INSTALLED_APPLICATIONS = 1;
-    public static int USER_INSTALLED_APPLICATIONS = 2;
-    public static int ALL_APPLICATIONS = 3;
+    public final static int SYSTEM_INSTALLED_APPLICATIONS = 1;
+    public final static int USER_INSTALLED_APPLICATIONS = 2;
+    public final static int ALL_APPLICATIONS = 3;
 
 
-    public static String NOT_PERMISSION_ENOUGH = "权限不足\n请打开手机设置，为Locker授权查看使用情况";
+    public final static String NOT_PERMISSION_ENOUGH = "权限不足\n请打开手机设置，为Locker授权查看使用情况";
 
 
-    public static int GET_APPINFO_ERROR = -1;
-    public static int GET_APPINFO_SUCCESS = 0;
+    public final static int GET_APPINFO_ERROR = -1;
+    public final static int GET_APPINFO_SUCCESS = 0;
+
+    public final static int UPDATE_LESSTIME = 1;
+    public final static int UPDATE_RULE_LESSTIME = 2;
+    public final static int UPDATE_CYCLETIME = 3;
+    public final static int UPDATE_RULE_CYCLETIME = 4;
 
 
 }
